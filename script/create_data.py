7ce1bdf1279c5766ace777622aae883909c1790e
Majid <majidad09@gmail.com>
2022-07-29 12:42:41 +0430

    Update to run with multi speaker data

--- script/create_data.py
+++ script/create_data.py
@@ -7,17 +7,24 @@ import argparse
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_path', required=True, default='data')
+    args = parser.parse_args()
     data_path = args.data_path
-    all_info = pd.read_csv(main_path + 'all_info.csv', dtype = str)
+    file_info_path = os.path.join(data_path, 'all_info.csv')
+    all_info = pd.read_csv(file_info_path , dtype = str)
+
     grouped = all_info.groupby('file_id')
     os.makedirs('../raw_data/', exist_ok=True)
     uttr_num = {}
     for name, group in grouped:
-        audio = AudioSegment.from_wav(main_path + 'wav/' + name + '.wav')
+        wav_path = os.path.join(data_path, 'wav', name + '.wav')
+        audio = AudioSegment.from_wav(wav_path)
         for index, row in group.iterrows():
             t1, t2 = float(row['start_time'])*1000, float(row['end_time'])*1000
             new_audio = audio[t1:t2]
-            speaker_id = str(int(row['speaker_id']))
+            if 'speaker_id' in row:
+                speaker_id = str(int(row['speaker_id']))
+            else:
+                speaker_id = '0'
             if speaker_id in uttr_num.keys():
                 uttr_num[speaker_id] += 1
             else:
@@ -25,6 +32,7 @@ def main():
             path = '../raw_data/spk_' + speaker_id
             os.makedirs(path, exist_ok=True)
             new_audio.export(path + '/uttr' + str(uttr_num[speaker_id]) + '.wav', format="wav")
-            
+
 if __name__ == "__main__":
     main()
+
